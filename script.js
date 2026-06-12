a3d7362acc297d8213cbd73fa487239275dd7925
DenisBode <Denis.Bode@gmx.de>
2026-06-12 16:00:07 +0200

    add trophy, styles for endScreen

--- script.js
+++ script.js
@@ -46,119 +46,120 @@ let questions = [
         "answer_3": "Strg + C",
         "answer_4": "Strg + Z",
         "right_answer": 3
-    },
-    {
-        "question": "Was ist ein Beispiel für ein Eingabegerät?",
-        "answer_1": "Monitor",
-        "answer_2": "Drucker",
-        "answer_3": "Maus",
-        "answer_4": "Lautsprecher",
-        "right_answer": 3
-    },
-    {
-        "question": "Was ist eine SSD?",
-        "answer_1": "Ein Bildschirm",
-        "answer_2": "Ein schneller Datenspeicher",
-        "answer_3": "Ein Prozessor",
-        "answer_4": "Ein Maustyp",
-        "right_answer": 2
-    },
-    {
-        "question": "Welches Protokoll wird für Webseiten genutzt?",
-        "answer_1": "FTP",
-        "answer_2": "SMTP",
-        "answer_3": "HTTP",
-        "answer_4": "POP3",
-        "right_answer": 3
-    },
-    {
-        "question": "Was ist Linux?",
-        "answer_1": "Ein Browser",
-        "answer_2": "Ein Open-Source-Betriebssystem",
-        "answer_3": "Ein Hardwarehersteller",
-        "answer_4": "Ein Dateiformat",
-        "right_answer": 2
-    },
-    {
-        "question": "Wie viele Bit sind ein Byte?",
-        "answer_1": "4",
-        "answer_2": "8",
-        "answer_3": "16",
-        "answer_4": "32",
-        "right_answer": 2
-    },
-    {
-        "question": "Was macht ein Antivirenprogramm?",
-        "answer_1": "Es beschleunigt das Internet",
-        "answer_2": "Es löscht alle Bilder",
-        "answer_3": "Es schützt vor Schadsoftware",
-        "answer_4": "Es reinigt die Tastatur",
-        "right_answer": 3
-    },
-    {
-        "question": "Was ist 'Google Chrome'?",
-        "answer_1": "Ein Webbrowser",
-        "answer_2": "Ein Betriebssystem",
-        "answer_3": "Eine Suchmaschine",
-        "answer_4": "Ein Texteditor",
-        "right_answer": 1
-    },
-    {
-        "question": "Welcher Anschluss wird oft für Monitore genutzt?",
-        "answer_1": "USB-A",
-        "answer_2": "HDMI",
-        "answer_3": "SATA",
-        "answer_4": "RJ45",
-        "right_answer": 2
-    },
-    {
-        "question": "Was ist eine Firewall?",
-        "answer_1": "Ein physischer Schutz gegen Feuer",
-        "answer_2": "Ein Netzwerksicherheits-System",
-        "answer_3": "Ein schneller Prozessor",
-        "answer_4": "Ein spezieller USB-Stick",
-        "right_answer": 2
-    },
-    {
-        "question": "Was bedeutet 'PDF'?",
-        "answer_1": "Personal Data File",
-        "answer_2": "Portable Document Format",
-        "answer_3": "Public Digital Folder",
-        "answer_4": "Printable Data Form",
-        "right_answer": 2
-    },
-    {
-        "question": "Welches Zeichen muss in jeder E-Mail-Adresse vorkommen?",
-        "answer_1": "#",
-        "answer_2": "&",
-        "answer_3": "@",
-        "answer_4": "$",
-        "right_answer": 3
-    },
-    {
-        "question": "Was ist der 'Desktop'?",
-        "answer_1": "Die Gehäuserückseite",
-        "answer_2": "Die Arbeitsfläche des Betriebssystems",
-        "answer_3": "Ein spezielles Kabel",
-        "answer_4": "Ein Computer-Virus",
-        "right_answer": 2
-    },
-    {
-        "question": "Wofür steht die Endung '.jpg'?",
-        "answer_1": "Ein Videoformat",
-        "answer_2": "Ein Textdokument",
-        "answer_3": "Ein Bildformat",
-        "answer_4": "Eine ausführbare Datei",
-        "right_answer": 3
-    },
-    {
-        "question": "Was ist ein Pixel?",
-        "answer_1": "Ein Bildpunkt",
-        "answer_2": "Eine Taste",
-        "answer_3": "Ein Kabel",
-        "answer_4": "Ein Speicherchip",
-        "right_answer": 1
     }
+    // },
+    // {
+    //     "question": "Was ist ein Beispiel für ein Eingabegerät?",
+    //     "answer_1": "Monitor",
+    //     "answer_2": "Drucker",
+    //     "answer_3": "Maus",
+    //     "answer_4": "Lautsprecher",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Was ist eine SSD?",
+    //     "answer_1": "Ein Bildschirm",
+    //     "answer_2": "Ein schneller Datenspeicher",
+    //     "answer_3": "Ein Prozessor",
+    //     "answer_4": "Ein Maustyp",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Welches Protokoll wird für Webseiten genutzt?",
+    //     "answer_1": "FTP",
+    //     "answer_2": "SMTP",
+    //     "answer_3": "HTTP",
+    //     "answer_4": "POP3",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Was ist Linux?",
+    //     "answer_1": "Ein Browser",
+    //     "answer_2": "Ein Open-Source-Betriebssystem",
+    //     "answer_3": "Ein Hardwarehersteller",
+    //     "answer_4": "Ein Dateiformat",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Wie viele Bit sind ein Byte?",
+    //     "answer_1": "4",
+    //     "answer_2": "8",
+    //     "answer_3": "16",
+    //     "answer_4": "32",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Was macht ein Antivirenprogramm?",
+    //     "answer_1": "Es beschleunigt das Internet",
+    //     "answer_2": "Es löscht alle Bilder",
+    //     "answer_3": "Es schützt vor Schadsoftware",
+    //     "answer_4": "Es reinigt die Tastatur",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Was ist 'Google Chrome'?",
+    //     "answer_1": "Ein Webbrowser",
+    //     "answer_2": "Ein Betriebssystem",
+    //     "answer_3": "Eine Suchmaschine",
+    //     "answer_4": "Ein Texteditor",
+    //     "right_answer": 1
+    // },
+    // {
+    //     "question": "Welcher Anschluss wird oft für Monitore genutzt?",
+    //     "answer_1": "USB-A",
+    //     "answer_2": "HDMI",
+    //     "answer_3": "SATA",
+    //     "answer_4": "RJ45",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Was ist eine Firewall?",
+    //     "answer_1": "Ein physischer Schutz gegen Feuer",
+    //     "answer_2": "Ein Netzwerksicherheits-System",
+    //     "answer_3": "Ein schneller Prozessor",
+    //     "answer_4": "Ein spezieller USB-Stick",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Was bedeutet 'PDF'?",
+    //     "answer_1": "Personal Data File",
+    //     "answer_2": "Portable Document Format",
+    //     "answer_3": "Public Digital Folder",
+    //     "answer_4": "Printable Data Form",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Welches Zeichen muss in jeder E-Mail-Adresse vorkommen?",
+    //     "answer_1": "#",
+    //     "answer_2": "&",
+    //     "answer_3": "@",
+    //     "answer_4": "$",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Was ist der 'Desktop'?",
+    //     "answer_1": "Die Gehäuserückseite",
+    //     "answer_2": "Die Arbeitsfläche des Betriebssystems",
+    //     "answer_3": "Ein spezielles Kabel",
+    //     "answer_4": "Ein Computer-Virus",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Wofür steht die Endung '.jpg'?",
+    //     "answer_1": "Ein Videoformat",
+    //     "answer_2": "Ein Textdokument",
+    //     "answer_3": "Ein Bildformat",
+    //     "answer_4": "Eine ausführbare Datei",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Was ist ein Pixel?",
+    //     "answer_1": "Ein Bildpunkt",
+    //     "answer_2": "Eine Taste",
+    //     "answer_3": "Ein Kabel",
+    //     "answer_4": "Ein Speicherchip",
+    //     "right_answer": 1
+    // }
 ];
 
 let currentQuestion = 0;
@@ -229,5 +230,12 @@ function resetAnswerButton() {
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 
+
+}
+
+function showEndSreenDetails() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    document.getElementById('correct-questions').innerHTML = questions.length;
 
 }

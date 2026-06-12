31bf34b615827ac4bcdf8f0da5b5079c3fd342fe
DenisBode <Denis.Bode@gmx.de>
2026-06-12 10:02:04 +0200

    ad id`s and function showQuestions()

--- script.js
+++ script.js
@@ -160,3 +160,22 @@ let questions = [
         "right_answer": 1
     }
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestions();
+}
+
+function showQuestions() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+}

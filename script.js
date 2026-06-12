657734f3ada99d21ffd5d183b4b6dda29f9fa8a1
DenisBode <Denis.Bode@gmx.de>
2026-06-12 14:21:37 +0200

    edit card body for endScreen, add function to end quiz after questions.length, delete logs, add function nextQuestion

--- script.js
+++ script.js
@@ -170,34 +170,64 @@ function init() {
 }
 
 function showQuestions() {
-    let question = questions[currentQuestion];
 
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionBody').style = 'display: none';
 
+    } else {
+
+        let question = questions[currentQuestion];
+
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    console.log('selected answer is:', selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log('selected Question Number is', selectedQuestionNumber);
-    console.log('current answer is', question['right_answer']);
-
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
     if (selectedQuestionNumber == question['right_answer']) {
-        console.log('richtige Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-success');
 
     } else {
-        console.log('falsche Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('next-button').disabled = false;
+
+}
+
+function nextQuestion() {
+    currentQuestion++; // z.B. von 0 auf 1
+    showQuestions(); // ruft die Funktion showQuestions auf
+
+    document.getElementById('next-button').disabled = true; // setzt die klickbarkeit des Buttons wieder auf klickbar
+
+    resetAnswerButton();
+
+}
+
+function resetAnswerButton() {
+
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+
 
 }
